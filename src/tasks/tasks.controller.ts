6998c6f2ed0d6cd734332f429a31fc3abd4a9009
Naveen-Kushalappa <tknaveen4@gmail.com>
2025-03-31 12:09:48 +0530

    Add tasks.controller with get, post, patch endpoints.

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
-
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task } from './tasks.model';
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+    
+    constructor(private readonly taskService: TasksService){}
+
+    @Get()
+    getAllTasks(): Task[] {
+        return this.taskService.getAllTasks();
+    }
+
+    @Get(':id')
+    getTaskById(@Param('id') id: string): Task {
+        return this.taskService.getTaskById(id);
+    }
+
+    @Post()
+    addTask(@Body('title') title: string, @Body('description') description : string): Task {
+        return this.taskService.createTask(title, description);
+    }
+
+    @Patch(':id')
+    updateTask(@Param('id') id: string, @Body('isDone') isDone: boolean): Task{
+        return this.taskService.updateTask(id, isDone);
+    }
+
+    @Patch(':id')
+    deleteTask(@Param('id') id: string): void {
+        this.taskService.deleteTask(id);
+    }
+}
