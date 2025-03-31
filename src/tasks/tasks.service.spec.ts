be8a8d131d19e706df9216a91a378153f4adf426
Naveen-Kushalappa <tknaveen4@gmail.com>
2025-03-31 12:44:10 +0530

    Add jest testing of tasks service.

--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -15,4 +15,46 @@ describe('TasksService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should create a task', () => {
+    const task = service.createTask('Test task 1', 'Test task 1 description');
+    expect (task).toBeDefined();
+    expect (task.title).toBeDefined();
+    expect (task.description).toBeDefined();
+    expect (task.id).toBeDefined();
+    expect (task.isDone).toBe(false);
+  })
+
+  it('should get all tasks', () => {
+    service.createTask('Test task1', 'Test desciption1');
+    service.createTask('Test task2', 'Test desciption2');
+
+    const tasks = service.getAllTasks();
+    expect (tasks).toBeDefined();
+    expect (tasks.length).toBe(2);
+  })
+
+  it('should update a task', () => {
+    const task = service.createTask('Test task1', 'Test task description');
+
+    const id = task.id;
+    const updatedTask = service.updateTask(id, true);
+
+    expect (updatedTask.isDone).toBe(true);
+  })
+
+  it('should get a task by id', () => {
+
+    const task = service.createTask('Test task1', 'Test task description');
+    const id = task.id;
+    expect (service.getTaskById(id).title === 'Test task1').toBe(true);
+  })
+
+  it('should delete a task by id', () => {
+    const task = service.createTask('Test task1', 'Test task description');
+    const id = task.id;
+    service.deleteTask(id);
+    expect (service.getTaskById(id)).toBeUndefined();
+  })
+
 });
